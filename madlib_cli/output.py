c75d1ac4a76a73a9e5006c44a09cd5bc908a448c
Anthony Lopez <Antl21292@gmail.com>
2023-03-27 18:55:38 -0400

    created 3 functions

--- madlib_cli/output.py
+++ madlib_cli/output.py
@@ -31,7 +31,55 @@ def read_template(file_path):
         return file.read()
 
 
+def get_descriptions(string):
+    split_string = string.split()
+    descriptors = []
+
+    for i in range(len(split_string)):
+        if "Adjective" in split_string[i]:
+            descriptors.append("Adjective")
+        elif "Noun" in split_string[i]:
+            descriptors.append("Noun")
+
+    descriptors = tuple(descriptors)
+    print(descriptors)
+    return descriptors
+
+
+def parse_template(string):
+    """
+    Function that takes in a madlib string template,
+    removes the contents of the user's
+    selections and returns a new,
+    hollowed copy for the user to fill out!
+    :param string: madlib template
+    :return: new madlib
+    """
+    descriptions = get_descriptions(string)
+    mod_string = str()
+
+    for item in descriptions:
+        if not mod_string and str(item) in string:
+            mod_string = string.replace(str(item), "")
+
+        elif mod_string and str(item) in mod_string:
+            mod_string = mod_string.replace(str(item), "")
+
+    print(mod_string)
+    # print(user_input)
+    return mod_string
+
+
 def merge(string, tpl):
-    return string.format(*tpl)
+    """
+    Function that takes in a string and a tuple or list
+    of strings, and formats the tuple or list into the string.
+    :param string: A string
+    :param tpl: A tuple OR list
+    :return: modified string
+    """
+    mod_string = string.format(*tpl)
+
+    return mod_string
 
 
